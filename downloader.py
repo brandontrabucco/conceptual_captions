0772decbf7521049b598ee754501aaa24f1bf35c
Brandon Trabucco <brandon@btrabucco.com>
2019-02-24 20:06:35 -0800

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -1,5 +1,5 @@
 """Author: Brandon Trabucco, Copyright 2019
-Extracts the google contextual captions dataset into tf records.
+Extracts the google contextual captions dataset into jpg files.
 MIT License"""
 
 import tensorflow as tf
@@ -90,51 +90,27 @@ def process_training_examples(training_example_queue, processed_queue, max_queue
         is_complete_flag[0] = len(training_example_queue) == 0 and no_more_inputs_flag[0]
         print("Finished processing a training example.")
 
-def to_sequence_example(training_example):
-    """Builds a SequenceExample proto for an image-caption pair.
-    Args: training_example: A TrainingExample object.
-    Returns: a string that represents the serialized sequence example."""
-    return tf.train.SequenceExample(
-        context=tf.train.Features(feature={
-            "image/image_id": tf.train.Feature(int64_list=tf.train.Int64List(value=[training_example.image_id])),
-            "image/data": tf.train.Feature(bytes_list=tf.train.BytesList(value=[training_example.data])),
-        }), feature_lists=tf.train.FeatureLists(feature_list={
-            "image/caption": tf.train.FeatureList(feature=[tf.train.Feature(bytes_list=tf.train.BytesList(
-                value=[bytes(word, "UTF-8")])) for word in training_example.caption])
-        })).SerializeToString()
-
-def serialize_training_examples(name, output_dir, num_examples_per_file, training_example_queue, num_files_so_far, num_examples_so_far, no_more_inputs_flag, is_complete_flag):
+def save_training_examples(name, output_dir, training_example_queue, num_examples_so_far, no_more_inputs_flag, is_complete_flag):
     """Downloads images from the internet based on the TSV file specified.
     Args: name: a string name to prepend to each file.
           output_dir: a string path to the folder where files shall be saved.
-          num_examples_per_file: an integer number of examples to save per file.
           training_example_queue: a queue of TrainingExamples to be processed.
-          num_files_so_far: an atomic wrapper around an integer count for the number of files that have been processed.
           num_examples_so_far: an atomic wrapper around an integer count of the number of training examples processed so far.
           no_more_inputs_flag: an atopmic wrapper around a boolean flag signaling no more inputs to process.
           is_complete_flag: an atopmic wrapper around a boolean flag signaling completion."""
     is_complete_flag[0] = False
     while not is_complete_flag[0]:
-        num_examples_in_this_file = 0
-        print("Starting to write a file.")
-        output_filename = "%s-%.7d.tfrecord" % (name, num_files_so_far[0])
-        num_files_so_far[0] += 1
-        writer = tf.python_io.TFRecordWriter(os.path.join(output_dir, output_filename))
-        while num_examples_in_this_file < num_examples_per_file and not is_complete_flag[0]:
-            while len(training_example_queue) == 0 and not no_more_inputs_flag[0]:
-                time.sleep(.5)
-            training_example = training_example_queue.pop(0)
-            serialized_example = to_sequence_example(training_example)
-            writer.write(serialized_example)
-            num_examples_so_far[0] += 1
-            print("Wrote {0} examples so far.".format(num_examples_so_far[0]))  
-            num_examples_in_this_file += 1
-            is_complete_flag[0] = len(training_example_queue) == 0 and no_more_inputs_flag[0]
-        writer.close()
-        sys.stdout.flush()
-        print("Finished writing a file.")
+        while len(training_example_queue) == 0 and not no_more_inputs_flag[0]:
+            time.sleep(.5)
+        training_example = training_example_queue.pop(0)
+        with open(os.path.join(output_dir, name + "{0}.jpg".format(
+            training_example.image_id, " ".join(training_example.caption))), "wb") as f:
+            f.write(training_example.data)
+        num_examples_so_far[0] += 1
+        print("Wrote {0} examples so far.".format(num_examples_so_far[0]))  
+        is_complete_flag[0] = len(training_example_queue) == 0 and no_more_inputs_flag[0]
 
-def process_dataset(tsv_filename, name, output_dir, num_examples_per_file, num_cores):
+def process_dataset(tsv_filename, name, output_dir, num_cores):
     """Serializes the dataset using the tsv entries
     Args: tsv_filename: string, the location to the TSV file.
           name: a string name to prepend to each file.
@@ -146,8 +122,8 @@ def process_dataset(tsv_filename, name, output_dir, num_examples_per_file, num_c
     tsv_thread = threading.Thread(target=process_tsv, args=(tsv_filename, queue_one, 2048, tsv_finished))
     processor_threads = [threading.Thread(target=process_training_examples, args=(queue_one, queue_two, 2048, 
         tsv_finished, processor_finished)) for i in range(num_cores)]
-    serializer_threads = [threading.Thread(target=serialize_training_examples, args=(name, output_dir, 
-        num_examples_per_file, queue_two, num_files_so_far, num_examples_so_far, processor_finished, serializer_finished)) for i in range(num_cores)]
+    serializer_threads = [threading.Thread(target=save_training_examples, args=(name, output_dir, 
+        queue_two, num_examples_so_far, processor_finished, serializer_finished)) for i in range(num_cores)]
     print("Starting all threads.")
     tsv_thread.start()
     for t in processor_threads + serializer_threads:
@@ -159,13 +135,12 @@ def process_dataset(tsv_filename, name, output_dir, num_examples_per_file, num_c
 tf.logging.set_verbosity(tf.logging.INFO)
 tf.flags.DEFINE_string("training_tsv", "./Train_GCC-training.tsv", "Path to the Training TSV file.")
 tf.flags.DEFINE_string("validation_tsv", "./Validation_GCC-1.1.0-Validation.tsv", "Path to the Validation TSV file.")
-tf.flags.DEFINE_string("output_dir", "./", "Output data directory.")
-tf.flags.DEFINE_integer("num_examples_per_file", 5096, "Number of examples per each TFRecord file.")
+tf.flags.DEFINE_string("output_dir", "./images/", "Output data directory.")
 tf.flags.DEFINE_integer("num_cores", 8, "Number of cores to use when extracting the dataset.")
 FLAGS = tf.flags.FLAGS
 
 if __name__ == "__main__":
     if not tf.gfile.IsDirectory(FLAGS.output_dir):
         tf.gfile.MakeDirs(FLAGS.output_dir)
-    process_dataset(FLAGS.training_tsv, "train", FLAGS.output_dir, FLAGS.num_examples_per_file, FLAGS.num_cores)
-    process_dataset(FLAGS.validation_tsv, "val", FLAGS.output_dir, FLAGS.num_examples_per_file, FLAGS.num_cores)
+    process_dataset(FLAGS.training_tsv, "train", FLAGS.output_dir, FLAGS.num_cores)
+    process_dataset(FLAGS.validation_tsv, "val", FLAGS.output_dir, FLAGS.num_cores)
